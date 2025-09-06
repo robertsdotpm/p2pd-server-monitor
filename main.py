c06b00672faf78ed46cc607e71350198c7ed4386
Matthew Roberts <matthew@roberts.pm>
2025-09-06 12:04:13 +1000

    Working on validating stun server type on add.

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ import asyncio
 import aiosqlite
 from p2pd import *
 
+
 DB_NAME = "monitor.sqlite3"
 
 #####################################################################################
@@ -77,9 +78,93 @@ async def is_unique_service(db, service_type, af, proto, ip, port):
         rows = await cursor.fetchall()
         return not len(rows)
     
+async def validate_stun_server(af, host, port, proto, interface, mode, timeout, recurse=True):
+    # Attempt to resolve STUN server address.
+    dest = await Address(host, port, interface)
+    dest = dest.select_ip(af)
+    
+    """
+    Some 'public' STUN servers like to point to private
+    addresses. This could be dangerous.
+    """
+    ipr = IPRange(dest.tup[0], cidr=af_to_cidr(af))
+    if ipr.is_private:
+        log(f"{af} {host} {recurse} is private")
+        return None
+
+    # New pipe used for the req.
+    stun_client = STUNClient(
+        af=af,
+        dest=dest.tup,
+        nic=interface,
+        proto=proto,
+        mode=mode
+    )
+
+    try:
+        reply = await stun_client.get_stun_reply()
+    except:
+        log_exception()
+        log(f"{af} {host} {proto} {mode} get reply {recurse} none")
+        return None
+    
+    reply = validate_stun_reply(reply, mode)
+    if reply is None:
+        return
+    # Cleanup.
+    if hasattr(reply, "pipe"):
+        await reply.pipe.close()  
+
+    # Validate change server reply.
+    ctup = (None, None)
+    if mode == RFC3489:
+        if recurse and hasattr(reply, "ctup"):
+            try:
+                # Change IP different from reply IP.
+                if reply.stup[0] == reply.ctup[0]:
+                    log(f'ctup bad {to_h(reply.txn_id)}: bad {reply.ctup[0]} 1')
+                    return None
+                
+                # Change port different from reply port.
+                if reply.stup[1] == reply.ctup[1]:
+                    log(f'ctup bad {to_h(reply.txn_id)}: bad {reply.ctup[0]} 2')
+                    return None
+
+
+                creply = await validate_stun_server(
+                    af,
+                    reply.ctup[0],
+                    reply.ctup[1],
+                    proto,
+
+                    interface,
+                    mode,
+                    timeout,
+                    recurse=False # Avoid infinite loop.
+                )
+            except:
+                log(f"vaid stun recurse failed {af} {host}")
+                log_exception()
+            if creply is not None:
+                ctup = reply.ctup
+            else:
+                log(f"{af} {host} {reply.ctup} ctup get reply {recurse} none")
+
+    # Return all the gathered data.
+    return [
+        af,
+        host,
+        dest.tup[0],
+        dest.tup[1], 
+        ctup[0],
+        ctup[1],
+        proto,
+        mode
+    ]
+    
 # Validation here.
 # Don't expose fallback_id directly in public APIs -- grouped service API though.
-async def record_service(db, service_type, af, proto, ip, port, fallback_id=None):
+async def record_service(db, nic, service_type, af, proto, ip, port, fallback_id=None):
     if not in_range(service_type, [1, 5]):
         raise Exception("Invalid service type for record")
     
@@ -102,17 +187,36 @@ async def record_service(db, service_type, af, proto, ip, port, fallback_id=None
     if not is_unq:
         raise Exception("service already inserted.")
     
+    # For stun validate the server type.
+    if service_type in (STUN_CHANGE_TYPE, STUN_MAP_TYPE):
+        try:
+            out = await validate_stun_server(
+                af,
+                ip,
+                port,
+                proto,
+                nic,
+                RFC5389 if service_type == STUN_MAP_TYPE else RFC3489,
+                8
+            )
+            print(out)
+        except:
+            what_exception()
+            pass
+
+    
     # Insert the service.
     ret = await insert_service(db, service_type, af, proto, ip, port, fallback_id)
     return ret
 
-async def insert_test_data(db):
+async def insert_test_data(db, nic):
     for groups in TEST_DATA:
         fallback_id = None
         print(groups)
         for group in groups:
             insert_id = await record_service(
                 db=db,
+                nic=nic,
                 service_type=group[1],
                 af=group[2],
                 proto=group[3],
@@ -123,9 +227,7 @@ async def insert_test_data(db):
 
             fallback_id  = insert_id
 
-async def worker(db):
-    nic = await Interface()
-
+async def worker(db, nic):
     # Try fetch a row
     async with db.execute("SELECT * FROM services ORDER BY id DESC;") as cursor:
         rows = await cursor.fetchall()
@@ -174,6 +276,14 @@ async def worker(db):
                     what_exception()
                     pass
 
+            if service_type == STUN_CHANGE_TYPE:
+                # Needs at least primary and secondary server IPs.
+                if len(groups) != 2:
+                    groups = []
+                    continue
+
+
+
             # Cleanup.
             if chain_end:
                 groups = []
@@ -188,15 +298,17 @@ async def main():
     print(int(V6)) # 10
     print(int(TCP))
     print(int(UDP))
+    nic = await Interface()
+
 
 
 
     params = (1, V4, 1, "127.0.0.1", 80, None)
     async with aiosqlite.connect(DB_NAME) as db:
         await delete_all_data(db)
-        await insert_test_data(db)
+        await insert_test_data(db, nic)
         #await get_last_row_id(db, "services")
-        await worker(db)
+        await worker(db, nic)
 
         await db.commit()
         return
